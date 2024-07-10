2725c67a18654e04d8e6c26661b67c3e5edef33a
Ramadhan <makbarramadhan62@gmail.com>
2024-07-10 15:07:08 +0700

    feat: add curiculum card component

---
+++ src/components/curiculumCard/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface CurriculumCardProps {
+  imageSrc: string;
+  imageAlt: string;
+  title: string;
+  description: string;
+}
+
+const CurriculumCard: React.FC<CurriculumCardProps> = ({
+  imageSrc,
+  imageAlt,
+  title,
+  description,
+}) => {
+  return (
+    <div className="card w-96 md:w-auto bg-transparent hover:bg-white hover:text-neutral rounded-lg hover-card">
+      <figure className="px-10 pt-10">
+        <img
+          height={100}
+          width={100}
+          style={{ width: "auto", height: "auto" }}
+          src={imageSrc}
+          alt={imageAlt}
+          className="object-contain mx-auto image-hover"
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="font-semibold text-2xl">{title}</h2>
+        <p className="text-lg font-normal">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CurriculumCard;
