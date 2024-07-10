47aff403c472786ef5175c3e299bb693b55f61dc
Ramadhan <makbarramadhan62@gmail.com>
2024-07-10 15:05:51 +0700

    feat: add school expense page

---
+++ src/pages/program-sekolah/biaya-sekolah/page.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import React, { SetStateAction, useEffect, useState } from "react";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+import Tab22 from "./tab22";
+import Tab21 from "./tab21";
+import Tab12 from "./tab12";
+import Tab11 from "./tab11";
+
+export default function SchoolExpenses() {
+  const [activeTab, setActiveTab] = useState(1);
+  const [activeTab2, setActiveTab2] = useState(1);
+  const [showWhatsApp, setShowWhatsApp] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowWhatsApp(true);
+    }, 800);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleTabChange = (tabIndex: SetStateAction<number>) => {
+    setActiveTab(tabIndex);
+  };
+  const handleTabChange2 = (tabIndex: SetStateAction<number>) => {
+    setActiveTab2(tabIndex);
+  };
+  return (
+    <div className="flex w-full flex-col items-center overflow-x-hidden">
+      {showWhatsApp && (
+        <FloatingWhatsApp
+          avatar="/image/logo.svg"
+          className="text-neutral"
+          phoneNumber="6285333392421"
+          accountName="Baitul Adab"
+          allowEsc={true}
+          chatMessage={"Halo, ada yang bisa dibantu?"}
+        />
+      )}
+
+      {/* First section */}
+      <section id="program-first" className="w-full mt-[92px]">
+        <div
+          className="w-full flex flex-col items-center justify-center h-[430px]"
+          style={{
+            backgroundImage:
+              "url(/image/program-sekolah/biaya-sekolah/headerBg.png)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="py-2 w-full bg-[#2B2B2BB2] flex flex-col items-center justify-center gap-2 text-center">
+            <span className="text-5xl font-bold text-secondary-content">
+              Biaya Pendaftaran
+            </span>
+            <span className="text-3xl font-bold">
+              BAITUL ADAB ISLAMIC SCHOOL
+            </span>
+            <span className="text-2xl font-bold">2024/2025</span>
+          </div>
+        </div>
+      </section>
+      {/* First section */}
+
+      {/* Second section */}
+      <section id="program-second" className="w-full my-8">
+        <div className="px-4 lg:px-16 text-center">
+          <span className="text-base-content text-2xl font-medium w-full lg:w-2/3">
+            Baitul Adab Islamic School mempunyai beberapa program sekolah,
+            berikut merupakan detail biaya setiap program sekolah:
+          </span>
+
+          <div className="flex gap-8 items-start font-semibold text-neutral mt-8">
+            <button
+              className={`${
+                activeTab === 1
+                  ? "text-blue-600 border-b-2 border-blue-600 pb-1"
+                  : "text-gray-600 hover:text-blue-600 hover:border-b-2 hover:border-blue-600 hover:pb-1"
+              }`}
+              onClick={() => handleTabChange(1)}
+            >
+              Playgroup Regular
+            </button>
+            <button
+              className={`${
+                activeTab === 2
+                  ? "text-blue-600 border-b-2 border-blue-600 pb-1"
+                  : "text-gray-600 hover:text-blue-600 hover:border-b-2 hover:border-blue-600 hover:pb-1"
+              }`}
+              onClick={() => handleTabChange(2)}
+            >
+              Playgroup Plus Daycare
+            </button>
+          </div>
+          <div className="w-full md:w-1/2 xl:w-1/4">
+            <hr />
+          </div>
+          {activeTab === 1 && <Tab11 />}
+          {activeTab === 2 && <Tab12 />}
+
+          <div className="flex gap-8 items-start font-semibold mt-8">
+            <button
+              className={`${
+                activeTab2 === 1
+                  ? "text-blue-600 border-b-2 border-blue-600 pb-1"
+                  : "text-gray-600 hover:text-blue-600 hover:border-b-2 hover:border-blue-600 hover:pb-1"
+              }`}
+              onClick={() => handleTabChange2(1)}
+            >
+              Kindegarten Regular
+            </button>
+            <button
+              className={`${
+                activeTab2 === 2
+                  ? "text-blue-600 border-b-2 border-blue-600 pb-1"
+                  : "text-gray-600 hover:text-blue-600 hover:border-b-2 hover:border-blue-600 hover:pb-1"
+              }`}
+              onClick={() => handleTabChange2(2)}
+            >
+              Kindegarten Plus Daycare
+            </button>
+          </div>
+          <div className="w-full md:w-1/2 xl:w-1/4">
+            <hr />
+          </div>
+          {activeTab2 === 1 && <Tab21 />}
+          {activeTab2 === 2 && <Tab22 />}
+        </div>
+      </section>
+      {/* Second section */}
+    </div>
+  );
+}
---
+++ src/pages/program-sekolah/biaya-sekolah/tab22.tsx
@@ -0,0 +1,126 @@
+const Tab22 = () => {
+  return (
+    <div className="mt-4 card p-10 bg-base-100">
+      <div className="flex flex-col items-center justify-between gap-4">
+        <span className="font-bold text-3xl lg:text-4xl text-center text-secondary-content">
+          Kindegarten Plus Daycare
+        </span>
+        <span className="font-normal text-base text-center text-neutral">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu
+        </span>
+      </div>
+
+      <div className="mt-10 flex flex-col gap-10 lg:flex-row items-start justify-center">
+        <div className="w-full xl:w-2/3 border border-primary-content rounded-md">
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th
+                    colSpan={2}
+                    className="text-center text-lg bg-primary-content bg-opacity-80 text-neutral"
+                  >
+                    BIAYA PLAYGROUP REGULAR (2-4 Years)
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="text-sm text-neutral font-medium">
+                {/* row 1 */}
+                <tr>
+                  <td>Biaya Registrasi</td>
+                  <td className="text-end">Rp. 100.000</td>
+                </tr>
+                {/* row 2 */}
+                <tr>
+                  <td>Biaya Masuk ( 1x Bayar )</td>
+                  <td className="text-end">Rp. 4.500.000</td>
+                </tr>
+                {/* row 3 */}
+                <tr>
+                  <td>Biaya Alat Tulis Sekolah</td>
+                  <td className="text-end">Rp. 225.000</td>
+                </tr>
+                {/* row 4 */}
+                <tr>
+                  <td>Biaya Aktifitas 1 Tahun</td>
+                  <td className="text-end">Rp. 1.760.000</td>
+                </tr>
+                {/* row 5 */}
+                <tr>
+                  <td>Biaya Seragam</td>
+                  <td className="text-end">Rp. 450.000</td>
+                </tr>
+                {/* row 6 */}
+                <tr>
+                  <td>Biaya Perbulan</td>
+                  <td className="text-end">Rp.1.450.000</td>
+                </tr>
+                {/* row 7 */}
+                <tr>
+                  <td>Biaya Test</td>
+                  <td className="text-end">Rp. 500.000</td>
+                </tr>
+                {/* row 8 */}
+                <tr className="font-bold text-secondary-content">
+                  <td>Total Pembayaran</td>
+                  <td className="text-end">Rp. 8.985.000</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="w-full xl:w-1/3 border border-primary-content rounded-md">
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th
+                    colSpan={2}
+                    className="text-center text-lg bg-primary-content bg-opacity-80 text-neutral"
+                  >
+                    JADWAL PLAYGROUP REGULAR
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="text-sm text-neutral font-medium">
+                {/* row 1 */}
+                <tr>
+                  <td>SENIN</td>
+                  <td className="text-end">07:00 - 16:00</td>
+                </tr>
+                {/* row 2 */}
+                <tr>
+                  <td>SELASA</td>
+                  <td className="text-end">07:00 - 16:00</td>
+                </tr>
+                {/* row 3 */}
+                <tr>
+                  <td>RABU</td>
+                  <td className="text-end">07:00 - 16:00</td>
+                </tr>
+                {/* row 4 */}
+                <tr>
+                  <td>KAMIS</td>
+                  <td className="text-end">07:00 - 16:00</td>
+                </tr>
+                {/* row 5 */}
+                <tr>
+                  <td>JUMAT</td>
+                  <td className="text-end">07:00 - 16:00</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tab22;
